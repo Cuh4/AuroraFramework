ad9cbfc24db0c1604adf8f27b475becbe630aa46
Cuh4 <roozzguy@gmail.com>
2023-07-31 15:12:48 +0100

    json library in httpservice from https://gist.github.com/tylerneylon/59f4bcf316be525b30ab

--- p0_framework/AuroraFramework.lua
+++ p0_framework/AuroraFramework.lua
@@ -1161,6 +1161,164 @@ AuroraFramework.services.HTTPService.cancel = function(port, url)
 	AuroraFramework.services.HTTPService.ongoingRequests[port.."|"..url] = nil
 end
 
+-- Source: https://gist.github.com/tylerneylon/59f4bcf316be525b30ab
+AuroraFramework.services.HTTPService.json = {}
+
+-- Internal
+AuroraFramework.services.HTTPService.json.kind_of = function(obj)
+	if type(obj) ~= "table" then return type(obj) end
+	local i = 1
+	for _ in pairs(obj) do
+		if obj[i] ~= nil then
+			i = i + 1
+		else
+			return 'table'
+		end
+	end
+	if i == 1 then
+		return 'table'
+	else
+		return 'array'
+	end
+end
+
+-- Internal
+AuroraFramework.services.HTTPService.json.escape_str = function(s)
+	local in_char = { '\\', '"', '/', '\b', '\f', '\n', '\r', '\t' }
+	local out_char = { '\\', '"', '/', 'b', 'f', 'n', 'r', 't' }
+	for i, c in ipairs(in_char) do s = s:gsub(c, '\\' .. out_char[i]) end
+	return s
+end
+
+-- Internal
+AuroraFramework.services.HTTPService.json.skip_delim = function(str, pos, delim)
+	pos = pos + #str:match('^%s*', pos)
+	if str:sub(pos, pos) ~= delim then
+		return pos, false
+	end
+	return pos + 1, true
+end
+
+-- Internal
+AuroraFramework.services.HTTPService.json.parse_str_val = function(str, pos, val)
+	val = val or ''
+	if pos > #str then return end
+	local c = str:sub(pos, pos)
+	if c == '"' then return val, pos + 1 end
+	if c ~= '\\' then return AuroraFramework.services.HTTPService.json.parse_str_val(str, pos + 1, val .. c) end
+	-- We must have a \ character.
+	local esc_map = { b = '\b', f = '\f', n = '\n', r = '\r', t = '\t' }
+	local nextc = str:sub(pos + 1, pos + 1)
+	if not nextc then return end
+	return AuroraFramework.services.HTTPService.json.parse_str_val(str, pos + 2, val .. (esc_map[nextc] or nextc))
+end
+
+-- Internal
+AuroraFramework.services.HTTPService.json.parse_num_val = function(str, pos)
+	local num_str = str:match('^-?%d+%.?%d*[eE]?[+-]?%d*', pos)
+	local val = tonumber(num_str)
+	if not val then return end
+	return val, pos + #num_str
+end
+
+-- Encode a Lua object into a JSON string
+---@param obj any The thing to encode
+---@param as_key boolean|nil
+---@return string
+AuroraFramework.services.HTTPService.json.encode = function(obj, as_key)
+	local s = {}           -- We'll build the string as an array of strings to be concatenated.
+	local kind = AuroraFramework.services.HTTPService.json.kind_of(obj) -- This is 'array' if it's an array or type(obj) otherwise.
+	if kind == 'array' then
+		if as_key then return end
+		s[#s + 1] = '['
+		for i, val in ipairs(obj) do
+			if i > 1 then s[#s + 1] = ',' end
+			s[#s + 1] = AuroraFramework.services.HTTPService.json.encode(val)
+		end
+		s[#s + 1] = ']'
+	elseif kind == 'table' then
+		if as_key then return end
+		s[#s + 1] = '{'
+		for k, v in pairs(obj) do
+			if #s > 1 then s[#s + 1] = ',' end
+			s[#s + 1] = AuroraFramework.services.HTTPService.json.encode(k, true)
+			s[#s + 1] = ':'
+			s[#s + 1] = AuroraFramework.services.HTTPService.json.encode(v)
+		end
+		s[#s + 1] = '}'
+	elseif kind == 'string' then
+		return '"' .. AuroraFramework.services.HTTPService.json.escape_str(obj) .. '"'
+	elseif kind == 'number' then
+		if as_key then return '"' .. tostring(obj) .. '"' end
+		return tostring(obj)
+	elseif kind == 'boolean' then
+		return tostring(obj)
+	elseif kind == 'nil' then
+		return 'null'
+	else
+		return
+	end
+	return table.concat(s)
+end
+
+-- Internal
+AuroraFramework.services.HTTPService.json.null = {}
+
+-- Decode a JSON string into a Lua object
+---@param str string
+---@param pos number|nil
+---@param end_delim string|nil
+---@return any, any
+AuroraFramework.services.HTTPService.json.decode = function(str, pos, end_delim)
+	pos = pos or 1
+	if pos > #str then return nil end
+	local pos = pos + #str:match('^%s*', pos) -- Skip whitespace.
+	local first = str:sub(pos, pos)
+	if first == '{' then                   -- Parse an object.
+		local obj, key, delim_found = {}, true, true
+		pos = pos + 1
+		while true do
+			key, pos = AuroraFramework.services.HTTPService.json.decode(str, pos, '}')
+			if key == nil then return obj, pos end
+			if not delim_found then return nil end
+			pos = AuroraFramework.services.HTTPService.json.skip_delim(str, pos, ':', true) -- true -> error if missing.
+			obj[key], pos = AuroraFramework.services.HTTPService.json.decode(str, pos)
+			pos, delim_found = AuroraFramework.services.HTTPService.json.skip_delim(str, pos, ',')
+		end
+	elseif first == '[' then -- Parse an array.
+		local arr, val, delim_found = {}, true, true
+		pos = pos + 1
+		while true do
+			val, pos = json.parse(str, pos, ']')
+			if val == nil then return arr, pos end
+			if not delim_found then return nil end
+			arr[#arr + 1] = val
+			pos, delim_found = AuroraFramework.services.HTTPService.json.skip_delim(str, pos, ',')
+		end
+	elseif first == '"' then                   -- Parse a string.
+		return AuroraFramework.services.HTTPService.json.parse_str_val(str, pos + 1)
+	elseif first == '-' or first:match('%d') then -- Parse a number.
+		return AuroraFramework.services.HTTPService.json.parse_num_val(str, pos)
+	elseif first == end_delim then             -- End of an object or array.
+		return nil, pos + 1
+	else                                       -- Parse true, false, or null.
+		local literals = {
+			['true'] = true,
+			['false'] = false,
+			['null'] = AuroraFramework.services.HTTPService.json.null
+		}
+		for lit_str, lit_val in pairs(literals) do
+			local lit_end = pos + #lit_str - 1
+			if str:sub(pos, lit_end) == lit_str then
+				return lit_val, lit_end + 1
+			end
+		end
+		local pos_info_str = 'position ' .. pos .. ': ' ..
+			str:sub(pos, pos + 10)
+		return nil
+	end
+end
+
 --------------------------------------------------------------------------------
 --// Messages \\--
 --------------------------------------------------------------------------------
