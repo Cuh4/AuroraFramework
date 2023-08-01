80950769847cdb5bcfee0d6f98e5e59d2f8e0941
Cuh4 <roozzguy@gmail.com>
2023-08-01 23:45:59 +0100

    fix maybe, dunno

--- p0_framework/AuroraFramework.lua
+++ p0_framework/AuroraFramework.lua
@@ -1184,7 +1184,7 @@ AuroraFramework.services.HTTPService.Base64.encode = function(data)
 	return ((data:gsub('.', function(x)
 		local r, b = '', x:byte()
 		for i = 8, 1, -1 do r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and '1' or '0') end
-		return r
+		return r;
 	end) .. '0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
 		if (#x < 6) then return '' end
 		local c = 0
@@ -1206,7 +1206,7 @@ AuroraFramework.services.HTTPService.Base64.decode = function(data)
 	end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
 		if (#x ~= 8) then return '' end
 		local c = 0
-		for i = 1, 8 do c = c + (x:sub(i, i) == '1' and 2 ^ (7 - i) or 0) end
+		for i = 1, 8 do c = c + (x:sub(i, i) == '1' and 2 ^ (8 - i) or 0) end
 		return string.char(c)
 	end))
 end
