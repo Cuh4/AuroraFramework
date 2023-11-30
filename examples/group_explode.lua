33aea9584a4e210badb0d455061e8e8aba6c7931
Cuh4 <roozzguy@gmail.com>
2023-11-30 13:15:34 +0000

    example 1

---
+++ examples/group_explode.lua
@@ -0,0 +1,27 @@
+------------------------------------------------------------------------------------------------------------------------
+    -- Aurora Framework | A reliable addon creation framework designed to make addon creation easier.
+	-- 		Created by @cuh6_ on Discord
+	--		My Discord: https://discord.gg/CymKaDE2pj
+------------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------
+-- [Example] This example explodes non-primary vehicles (any vehicles that weren't the first to spawn in a group) in a group that just spawned.
+----------------------------------------
+
+-- // Attach a function to Group Service's onSpawn event
+---@param group af_services_group_group
+AuroraFramework.services.groupService.events.onSpawn:connect(function(group)
+    -- Loop through all vehicles in the group
+    for _, vehicle in pairs(group.properties.vehicles) do
+        -- This is a primary vehicle, so we ignore it
+        if vehicle.properties.isPrimaryVehicle then
+            goto continue
+        end
+
+        -- This is a non-primary vehicle, so let's explode it
+        vehicle:explode(0.05, false) -- Magnitude of 0.05 / 1, despawn set to false so the vehicle doesn't despawn upon exploding
+
+        -- Continue to next vehicle
+        ::continue::
+    end
+end)
