42be36eb4eaa4ddd4b21c6d8a718afea33eb1aab
Cuh4 <roozzguy@gmail.com>
2023-12-12 15:50:46 +0000

    new examples

---
+++ examples/addon_communication.lua
@@ -0,0 +1,33 @@
+------------------------------------------------------------------------------------------------------------------------
+    -- Aurora Framework | A reliable addon creation framework designed to make addon creation easier.
+	-- 		Created by @cuh6_ on Discord
+	--		My Discord: https://discord.gg/CymKaDE2pj
+------------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------
+-- [Example] Addon Communication
+-- This example shows how you can communicate between addons easily with this framework
+----------------------------------------
+
+-- Create a channel
+-- The name of the channel cannot have spaces, and for other addons to receive messages from this addon, they have to use the same channel name when creating a channel
+local mainChannel = AuroraFramework.services.communicationService.createChannel("main")
+
+-- Listen for messages on the channel
+mainChannel:listen(function(data)
+    AuroraFramework.services.chatService.sendMessage("Server", "I received a message: "..data["message"]) -- this will print in chat: "I received a message: hello world"
+end)
+
+-- Send a message to the channel
+mainChannel:send({ -- note that this can even be a string. it doesn't have to be a table
+    message = "hello world",
+
+    data = {
+        "anything",
+        "can go",
+        "here",
+        {
+            "and it'll be sent over"
+        }
+    }
+})
---
+++ examples/commands.lua
@@ -0,0 +1,28 @@
+------------------------------------------------------------------------------------------------------------------------
+    -- Aurora Framework | A reliable addon creation framework designed to make addon creation easier.
+	-- 		Created by @cuh6_ on Discord
+	--		My Discord: https://discord.gg/CymKaDE2pj
+------------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------
+-- [Example] Commands
+-- This example creates a one-use command that kills the user. Only players with auth can use the command.
+----------------------------------------
+
+-- Create a command that kills any player who uses it, and prevents others from using it afterwards.
+---@param command af_services_command_command
+---@param player af_services_player_player
+---@param args table<integer, string>
+AuroraFramework.services.commandService.create(function(command, player, args)
+    player:kill() -- kills the player
+    command:successNotification("You died! Congratulations!", player) -- shows a success notification on the right of their screen temporarily
+
+    command:remove() -- prevent other players from using this command
+end, "kill", {"s", "k", "suicide", "die"}, false, "Kills the player.", true, false)
+-- The parameters passed through to the "create" function are, in order, the following:
+-- name ("kill"),
+-- shorthands ({"s", "k", "suicide", "die"}),
+-- capsSensitive (false),
+-- description ("Kills the player."),
+-- requiresAuth (true),
+-- requiresAdmin (false)
