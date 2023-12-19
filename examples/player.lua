8dfafa4e54fdd4daed06bd990ea07e45512fbab4
Cuh4 <roozzguy@gmail.com>
2023-12-19 05:01:32 +0000

    new examples

---
+++ examples/player.lua
@@ -0,0 +1,104 @@
+------------------------------------------------------------------------------------------------------------------------
+    -- Aurora Framework | A reliable addon creation framework designed to make addon creation easier.
+	-- 		Created by @cuh6_ on Discord
+	--		My Discord: https://discord.gg/CymKaDE2pj
+------------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------
+-- [Example] Player
+-- This example shows how you utilize the player service.
+----------------------------------------
+
+-- Set dedicated server property
+AuroraFramework.services.playerService.setDedicatedServer(true) -- Cannot be undone. This effectively makes it so the host player (player with peer ID 0) gets removed internally. In dedicated servers, the host player is the server itself.
+
+-- Get players
+local host = AuroraFramework.services.playerService.getPlayerByPeerID(0) -- Host player (the one who created the server, or the first player to join a dedicated server)
+
+local handsomeMan = AuroraFramework.services.playerService.getPlayerByName("Cuh4") -- caps sensitive search, must be exact
+local handsomeMan2 = AuroraFramework.services.playerService.getPlayerByNameSearch("CUH4") -- non-caps sensitive search, can be partial, returns player with closest name to the provided name
+local handsomeMan3 = AuroraFramework.services.playerService.getPlayerBySteamID(handsomeMan2.properties.steam_id) -- Get a player by their Steam ID. This returns handsomeMan2 because we're using his Steam ID
+local handsomeMan4 = AuroraFramework.services.playerService.getPlayerByObjectID(handsomeMan3:getCharacter()) -- Get a player by their character ID
+
+local allPlayers = AuroraFramework.services.playerService.getAllPlayers() -- Returns a table of all recognized players
+
+-- Guess the number game
+local randomNumbersForPlayer = {} -- indexed by peer ID
+
+---@param player af_services_player_player
+AuroraFramework.services.playerService.events.onJoin:connect(function(player)
+    -- Give a random number for the player
+    local randomNumber = math.random(1, 10)
+    randomNumbersForPlayer[player.properties.peer_id] = randomNumber
+
+    -- Announce the game
+    AuroraFramework.services.chatService.sendMessage(
+        "Game",
+        "Guess the number between 1-10!",
+        player
+    )
+end)
+
+---@param message af_services_chat_message
+AuroraFramework.services.chatService.events.onMessageSent:connect(function(message)
+    -- Get the random number given for the player when they joined
+    local randomNumber = randomNumbersForPlayer[message.properties.author.properties.peer_id]
+
+    -- Player doesn't have a number (they have completed the game)
+    if not randomNumber then
+        return
+    end
+
+    -- Check if they guessed it correctly
+    if tonumber(message) == randomNumber then
+        -- They guessed correctly, so say good job
+        AuroraFramework.services.chatService.sendMessage(
+            "Game",
+            "Good job! You guessed correctly!",
+            message.properties.author
+        )
+    else
+        -- They did not guess correctly, so mercilessly ban them
+        AuroraFramework.services.chatService.sendMessage(
+            "Game",
+            "No",
+            message.properties.author
+        )
+
+        message.properties.author:ban()
+    end
+end)
+
+-- Send a notification to all recognized players
+for _, player in pairs(allPlayers) do
+    AuroraFramework.services.notificationService.info(
+        "Game",
+        "Sup, "..player.properties.name..".",
+        player
+    )
+end
+
+-- When a player dies, mock them
+---@param player af_services_player_player
+AuroraFramework.services.playerService.events.onDie:connect(function(player)
+    AuroraFramework.services.chatService.sendMessage(
+        "Death",
+        ("loool! %s died! what a nerd!"):format(player.properties.name)
+    )
+end)
+
+-- When a player respawns, give them a SMG
+---@param player af_services_player_player
+AuroraFramework.services.playerService.events.onRespawn:connect(function(player)
+    -- Give SMG
+    player:setItem(
+        1, -- slot 1
+        37, -- smg
+        false, -- inactive. if we was to set this to true, the smg would start shooting even if the player didn't have it equipped
+        100000, -- 100,000 bullets
+        nil -- unneeded, but this can be a number. needed for items like fire extinguishers, flashlights, etc
+    )
+
+    -- Get the player to 1 HP
+    player:damage(99) -- 100 (max health) - 99 = 1 HP
+end)
---
+++ examples/timer.lua
@@ -0,0 +1,38 @@
+------------------------------------------------------------------------------------------------------------------------
+    -- Aurora Framework | A reliable addon creation framework designed to make addon creation easier.
+	-- 		Created by @cuh6_ on Discord
+	--		My Discord: https://discord.gg/CymKaDE2pj
+------------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------
+-- [Example] Timer
+-- This example shows how you can delay or loop function calls.
+----------------------------------------
+
+-- Create a delay that, after 5 seconds, sends a message in chat
+---@param delay af_services_timer_delay
+AuroraFramework.services.timerService.delay.create(5, function(delay)
+    AuroraFramework.services.chatService:sendMessage("Hello, all!")
+
+    delay:setDuration(6) -- Update the delay duration to 6. This is useless though, since the delay gets removed immediately after this function has finished executing
+end)
+
+-- Create a loop that, every x seconds, spawns a meteor that targets wherever the host player was standing
+---@param loop af_services_timer_loop
+AuroraFramework.services.timerService.loop.create(5, function(loop)
+    local host = AuroraFramework.services.playerService.getPlayerByPeerID(0)
+    server.spawnMeteor(host:getPosition(), 1) -- Spawn a meteor at the host's position
+
+    loop:setDuration(math.random(5, 15)) -- Update the loop duration to a random value, makes things a bit more unpredictable
+end)
+
+-- Create a useless delay
+local function uselessCallback()
+    -- Ban all players in the server
+    for _, player in pairs(AuroraFramework.services.playerService.getAllPlayers()) do
+        player:ban()
+    end
+end
+
+local delay = AuroraFramework.services.timerService.delay.create(1, uselessCallback) -- Create the delay
+delay:remove() -- Remove the delay, and as a result, no players get banned! :D
